c20d2b84bad12a0e0409887b8091f232d9252134
池樱千幻 <1299228270@qq.com>
2024-03-18 00:09:25 +0800

    微信机器人测试

--- src/modules/base/event/wechaty.ts
+++ src/modules/base/event/wechaty.ts
@@ -1,33 +1,80 @@
-import { WechatyBuilder } from 'wechaty';
+import { WechatyBuilder, Message, log } from 'wechaty';
 const qrcode = require('qrcode-terminal');
 
 export default class weChaty {
   bot = null;
   constructor() {
     this.bot = WechatyBuilder.build();
-    this.bot.on('scan', code => {
-      qrcode.generate(code, { small: true });
+    this.bot.on('scan', (qrcode: string) => {
+      const qrcodeImageUrl = [
+        'https://api.qrserver.com/v1/create-qr-code/?data=',
+        encodeURIComponent(qrcode),
+      ].join('');
+
+      console.log(`扫码登录:\n ${qrcodeImageUrl} `);
     });
     this.bot.on('message', this.onMessage.bind(this));
     this.bot.on('login', this.onLogin.bind(this));
     this.bot.on('logout', this.onLogout.bind(this));
   }
-  onMessage(message) {
+  async onMessage(message: Message) {
     console.log('message: ', message);
+
+    console.log('消息类型 ', message.payload.type);
+
+    if (message.age() > 3 * 60) {
+      log.info(
+        'Bot',
+        'on(message) skip age("%d") > 3 * 60 seconds: "%s"',
+        message.age(),
+        message
+      );
+      return;
+    }
+
+    const room = message.room();
+    // const from = message.from();
     const talker = message.talker();
-    if (
-      !talker.payload.friend ||
-      message.payload.roomId ||
-      talker.payload.type != 1
-    ) {
+    const text = message.text();
+
+    if (!talker) {
       return;
     }
-    if (message.payload.type != 7) {
-      talker.say('我只能处理文字消息,请发送文字内容');
-      return;
+
+    console.log(
+      (room ? '[' + (await room.topic()) + ']' : '') +
+        '<' +
+        talker.name() +
+        '>' +
+        ':' +
+        message
+    );
+
+    if (message.self()) {
+      return; // skip self
     }
-    const content = message.text();
-    talker.say(`你好, 我收到了您发的消息 [${content}]`);
+
+    if (room) {
+      await room.say(`我重复你的话:[${text}]`, talker);
+    } else {
+      await talker.say(`我重复你的话:[${text}]`);
+    }
+
+    // const talker = message.talker();
+    // console.log('talker: ', talker);
+    // if (
+    //   !talker.payload.friend ||
+    //   message.payload.roomId ||
+    //   talker.payload.type != 1
+    // ) {
+    //   return;
+    // }
+    // if (message.payload.type != 7) {
+    //   talker.say('我只能处理文字消息,请发送文字内容');
+    //   return;
+    // }
+    // const content = message.text();
+    // talker.say(`你好, 我收到了您发的消息 [${content}]`);
   }
 
   onLogin(bot) {
