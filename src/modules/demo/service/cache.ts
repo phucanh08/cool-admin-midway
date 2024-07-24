9b5f8c8445791acbb71a577a3207597d7481ce07
Mirai Zhao <miraizhao@qq.com>
2024-07-24 11:38:40 +0800

    fix: 修改demo-ervice-cache装饰器的传参为5000，与注释5秒一致

--- src/modules/demo/service/cache.ts
+++ src/modules/demo/service/cache.ts
@@ -7,7 +7,7 @@ import { CoolCache } from '@cool-midway/core';
 @Provide()
 export class DemoCacheService {
   // 数据缓存5秒
-  @CoolCache(5)
+  @CoolCache(5000)
   async get() {
     console.log('执行方法');
     return {
